ab15fc2ee0285978a9d706ee8c2e713fa30e719d
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-02-02 20:59:20 +0200

    database done

--- site_retete/site_retete/settings.py
+++ site_retete/site_retete/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'site_retete.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'retete',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- site_retete/site_retete_app/views.py
+++ site_retete/site_retete_app/views.py
@@ -15,11 +15,11 @@ def lista_retete(request):
 
 
 def home(request):
-    # retete = Reteta.objects.all()
-    retete = [
-        {'nume': 'Pui cu legume', 'timp': '30 minute', 'dificultate': 'Ușor', 'ingrediente': 'Pui, legume'},
-        {'nume': 'Paste Carbonara', 'timp': '20 minute', 'dificultate': 'Mediu',
-         'ingrediente': 'Paste, ou, bacon, parmezan'},
-        # Adăugați mai multe rețete aici
-    ]
+    retete = Reteta.objects.all()
+    # retete = [
+    #     {'nume': 'Pui cu legume', 'timp': '30 minute', 'dificultate': 'Ușor', 'ingrediente': 'Pui, legume'},
+    #     {'nume': 'Paste Carbonara', 'timp': '20 minute', 'dificultate': 'Mediu',
+    #      'ingrediente': 'Paste, ou, bacon, parmezan'},
+    #     # Adăugați mai multe rețete aici
+    # ]
     return render(request, "home.html", {'retete': retete})
